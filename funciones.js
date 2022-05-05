d79cb273d089ea058e6304b19541bb7b307c9c54
Xeadnor <104887544+Xeadnor@users.noreply.github.com>
2022-05-05 12:29:38 +0200

    ejercicios js

---
+++ funciones.js
@@ -0,0 +1,91 @@
+
+function comprobarDato(dato) {
+
+    if (dato == "true" | dato == "false") {
+        dato = dato + " Es booleano";
+    } else if (!isNaN(dato)) {
+        dato = dato + " Es un numero"
+    } else {
+        dato = dato + " Es una cadena"
+    }
+    return dato;
+}
+
+//------
+
+function suma(v1, v2) {
+    return v1 + v2;
+}
+
+//------
+function mayor(v1, v2) {
+    return v1 > v2 ? v1 : v2
+}
+
+//------
+function parImpar(v1) {
+    return v1 % 2 == 0 ? "par" : "impar"
+}
+
+//------
+function menor(cantidad) {
+    let Numeros = new Array(cantidad);
+
+    for (let index = 0; index < Numeros.length; index++) {
+        Numeros[index] = (parseInt(prompt("Introduce valor")))
+    }
+    let menor = Numeros[0]
+    for (let index = 0; index < Numeros.length; index++) {
+        let valor = Numeros[index]
+
+        if (menor > valor) {
+            menor = valor;
+        }
+    }
+    return menor;
+}
+
+
+//------
+
+function central(cantidad, mitad) {
+    let Numeros = new Array(cantidad);
+
+    for (let index = 0; index < Numeros.length; index++) {
+        Numeros[index] = (parseInt(prompt("Introduce valor")))
+    }
+    let central = Numeros[mitad]
+
+    return central;
+}
+
+
+    //------
+
+
+    function ordenar(n){
+
+    // opcion rapida con n.sort()
+
+    for (let i = 0; i < n.length; i++) {
+        for (let j = i + 1; j < n.length; j++) {
+            if (n[i] > n[j]) {
+                let auxiliar = n[i]
+                n[i] = n[j]
+                n[j] = auxiliar
+
+            }
+
+        }
+    }
+    return n;
+}
+
+
+function media(n){
+   let total = 0;
+    for (let i = 0; i < n.length; i++) {
+       let total = total + n[index]
+    }
+    return total
+}
