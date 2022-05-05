29fa7e9ea8d1584f192fa8e868ca546726c023a5
email <email@gmail.com>
2022-05-05 13:16:00 +0200

    actualizacion 5/5

--- funciones.js
+++ funciones.js
@@ -82,10 +82,28 @@ function central(cantidad, mitad) {
 }
 
 
-function media(n){
+ function media(n){
    let total = 0;
-    for (let i = 0; i < n.length; i++) {
-       let total = total + n[index]
-    }
-    return total
+   for (let i = 0; i < numeros.length; i++) {
+    total = total + numeros[i]
+ }
+    return (total/n.length)
+}
+
+function imc(estatura,peso){
+let imc = peso/(estatura^2)
+let resultado = "";
+
+if (imc < 18.5){
+    resultado = "Peso inferior al normal"
+}else if (imc >= 18.5 && imc <= 24.9) {
+    resultado = "normal"
+}
+else if (imc >= 25 && imc <= 29.9) {
+    resultado = "Peso superior al normal"
+}
+else if (imc > 30) {
+    resultado = "Obesidad"
+}
+return resultado;
 }
